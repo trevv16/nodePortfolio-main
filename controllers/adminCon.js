164aa494887ad3803410f0fe2a1a0c0e5e38c697
Trevor Njeru <tnjeru16@gmail.com>
2020-05-09 10:19:39 -0400

    linted w/ prettier

--- controllers/adminCon.js
+++ controllers/adminCon.js
@@ -1,388 +1,284 @@
-
 var mongoose = require("mongoose");
 
+// MODELS
 var User = require("../models/user.js");
 var Project = require("../models/project.js");
 var Blog = require("../models/blog.js");
 var Post = require("../models/post.js");
 
+// CONTROLLERS
+// var projCon = require("./projCon.js");
 
 // GET Dash Function
-exports.getDash = function(req, res, next) { 
-  var foundprojects = [];
-  var viewtype;
-  
-  if(req.query.viewtype) {
-    viewtype = req.query.viewtype;
-  }
-  else {
-    viewtype = "card";
-  }
-  
-  
-  Project.find()
-  
-  .then(function(proj) {
-      
-      if (proj) {
-          foundprojects = proj;
-      }
-  
-  })
-  .catch( function(err) {
-      if(err){
-          console.log('DB Error: ' + err);
-      }
-  });
-  
-  res.render('admin/dash', {Title: "Dashboard", Count: foundprojects.length, Project: foundprojects, View: viewtype});
-  
+exports.getDash = function(req, res, next) {
+  var me = this;
+  var viewtype = (req.query.viewtype) ? viewtype = req.query.viewtype : viewtype = "card";
+  var foundprojects = me.getProjects;
+  console.log('found: ', foundprojects);
+  // Count: foundprojects.length,
+  res.render('admin/dash', {Title: "Dashboard", Work: foundprojects, View: viewtype});
 }
 
-
-//////////////// ADMIN PROJECT FUNCTIONS /////////////////////////////////////// 
-
+//////////////// ADMIN PROJECT FUNCTIONS ///////////////////////////////////////
 
 // GET New Project Function
-exports.newProject = function(req, res, next) { 
-    
-    
+exports.getProjects = function() {
+  var projects;
+  Project.find()
+  .then((doc) => {
+      if (doc) {
+          projects = doc;
+      }
+  })
+  .catch((err) => {
+      if(err){
+          console.log('DB Error: ',err);
+      }
+  });
+
+  return projects;
+}
+
+// GET New Project Function
+exports.newProject = function(req, res, next) {
     res.render('projects/new', { Title: 'New Project' });
 }
 
 // POST Add Project Function
-exports.addProject = function(req, res, next) { 
-  
-    var name = req.body.name;
-    var date = req.body.date;
-    var link = req.body.link;
-    var img = req.body.img;
-    var imgFile;
-    var desc = req.body.desc;
-    var demo = req.body.demo;
-    var code = req.body.code;
-    var gallery_imgs = [];
-    var skills = req.body.skills;
-    
-    
-    function getSkill(skillname) {
-        
-        var skName;
-        
-        switch (skillname.toLowerCase()) {
-            
-            case 'html':
-                skName = "https://elasticbeanstalk-us-east-2-257945705175.s3.us-east-2.amazonaws.com/portfolioimages/html.png";
-                gallery_imgs.push(skName);
-            break;
-            
-            case 'css':
-                skName = "https://elasticbeanstalk-us-east-2-257945705175.s3.us-east-2.amazonaws.com/portfolioimages/css.png";
-                gallery_imgs.push(skName);
-            break;
-            
-            case 'javascript':
-                skName = "https://elasticbeanstalk-us-east-2-257945705175.s3.us-east-2.amazonaws.com/portfolioimages/js.png";
-                gallery_imgs.push(skName);
-            break;
-            
-            case 'java':
-                skName = "https://elasticbeanstalk-us-east-2-257945705175.s3.us-east-2.amazonaws.com/portfolioimages/java.png";
-                gallery_imgs.push(skName);
-            break;
-            
-            case 'jquery':
-                skName = "https://elasticbeanstalk-us-east-2-257945705175.s3.us-east-2.amazonaws.com/portfolioimages/jqueryIcon.png";
-                gallery_imgs.push(skName);
-            break;
-            
-            case 'node js':
-                skName = "https://elasticbeanstalk-us-east-2-257945705175.s3.us-east-2.amazonaws.com/portfolioimages/node.png";
-                gallery_imgs.push(skName);
-            break;
-            
-            case 'bootstrap':
-                skName = "https://elasticbeanstalk-us-east-2-257945705175.s3.us-east-2.amazonaws.com/portfolioimages/bootstrap.png";
-                gallery_imgs.push(skName);
-            break;
-            
-            case 'mongodb':
-                skName = "https://elasticbeanstalk-us-east-2-257945705175.s3.us-east-2.amazonaws.com/portfolioimages/mongodb.png";
-                gallery_imgs.push(skName);
-            break;
-            
-            case 'sql':
-                skName = "https://elasticbeanstalk-us-east-2-257945705175.s3.us-east-2.amazonaws.com/portfolioimages/sql.png";
-                gallery_imgs.push(skName);
-            break;
-            
-            
-            default:
-                // code
-                
-            break;
-        }
-        
-    }
-    
-    
-    
-    for (var i = 0; i < skills.length; i++ ) {
-        
-        getSkill(skills[i]);
-        
-    }
-    
-    
-    // Check for image upload
-    if(req.file) {
-        
-        imgFile = req.file;
-        
-        console.log("Image File ",imgFile);
-    }
-    else throw 'error';
+exports.addProject = function(req, res, next) {
+  var name = req.body.name;
+  var date = req.body.date;
+  var link = req.body.link;
+  var img = req.body.img;
+  var imgFile;
+  var desc = req.body.desc;
+  var demo = req.body.demo;
+  var code = req.body.code;
+  var gallery_imgs = [];
+  var skills = req.body.skills;
 
-  
-    // Create new Project
-  
-    var genProject = new Project({
-      
-      _id: new mongoose.Types.ObjectId(),
-      name: name,
-      pub_date: date,
-      proj_link: link,
-      proj_img: img,
-      proj_desc: desc,
-      skills_used: gallery_imgs,
-      demo_link: demo,
-      code_link: code
-      // Add Created Date
-      
-    });
-    
-    console.log(genProject);
-    
-    // genProject.save()
-    // .then( function(result) {
-        
-    //     if(result){
-    //         console.log('DB Result:' + result);
-    //     }
-    // })
-    // .catch( function(err) {
-    //     if(err){
-    //         console.log('DB Error:' + err);
-    //     }
-    // });
+  function getSkill(skillname) {
+      var skName;
 
-    console.log('Saved New Project');
-  
-  
-    res.redirect('/projects');  
-    
-    
+      switch (skillname.toLowerCase()) {
+          case 'html':
+              skName = "https://elasticbeanstalk-us-east-2-257945705175.s3.us-east-2.amazonaws.com/portfolioimages/html.png";
+              gallery_imgs.push(skName);
+          break;
+
+          case 'css':
+              skName = "https://elasticbeanstalk-us-east-2-257945705175.s3.us-east-2.amazonaws.com/portfolioimages/css.png";
+              gallery_imgs.push(skName);
+          break;
+
+          case 'javascript':
+              skName = "https://elasticbeanstalk-us-east-2-257945705175.s3.us-east-2.amazonaws.com/portfolioimages/js.png";
+              gallery_imgs.push(skName);
+          break;
+
+          case 'java':
+              skName = "https://elasticbeanstalk-us-east-2-257945705175.s3.us-east-2.amazonaws.com/portfolioimages/java.png";
+              gallery_imgs.push(skName);
+          break;
+
+          case 'jquery':
+              skName = "https://elasticbeanstalk-us-east-2-257945705175.s3.us-east-2.amazonaws.com/portfolioimages/jqueryIcon.png";
+              gallery_imgs.push(skName);
+          break;
+
+          case 'node js':
+              skName = "https://elasticbeanstalk-us-east-2-257945705175.s3.us-east-2.amazonaws.com/portfolioimages/node.png";
+              gallery_imgs.push(skName);
+          break;
+
+          case 'bootstrap':
+              skName = "https://elasticbeanstalk-us-east-2-257945705175.s3.us-east-2.amazonaws.com/portfolioimages/bootstrap.png";
+              gallery_imgs.push(skName);
+          break;
+
+          case 'mongodb':
+              skName = "https://elasticbeanstalk-us-east-2-257945705175.s3.us-east-2.amazonaws.com/portfolioimages/mongodb.png";
+              gallery_imgs.push(skName);
+          break;
+
+          case 'sql':
+              skName = "https://elasticbeanstalk-us-east-2-257945705175.s3.us-east-2.amazonaws.com/portfolioimages/sql.png";
+              gallery_imgs.push(skName);
+          break;
+
+          default:
+              // code
+          break;
+      }
+  }
+
+  for (var i = 0; i < skills.length; i++ ) {
+    getSkill(skills[i]);
+  }
+
+
+  // Check for image upload
+  if(req.file) {
+    imgFile = req.file;
+
+    console.log("Image File ",imgFile);
+  }
+  else throw 'error';
+
+  /********************
+   *
+   *
+   *
+   *
+   *
+   *
+   * HANDLE S3 STORAGE, RETRIEVE LINK
+   *
+   *
+   *
+   *
+   *
+   *
+   * **********************/
+
+  // Create new Project
+  var genProject = new Project({
+    _id: new mongoose.Types.ObjectId(),
+    name: name,
+    pub_date: date,
+    proj_link: link,
+    proj_img: img,
+    proj_desc: desc,
+    skills_used: gallery_imgs,
+    demo_link: demo,
+    code_link: code
+  });
+
+  genProject.save()
+  .then( function(result) {
+    if(result){
+      console.log('DB Result:' + result);
+    }
+  })
+  .catch( function(err) {
+    if(err){
+      console.log('DB Error:' + err);
+    }
+  });
+
+  console.log('Saved New Project');
+  res.redirect('/projects');
 }
 
 
 // GET Edit Project Function
-exports.editProject = function(req, res, next) { 
-    
-    var query = req.params._id;
-  
-    Project.find({ _id: query})
-    
+exports.editProject = function(req, res, next) {
+    Project.find({ _id: req.params._id})
     .then( function(result) {
-        
         if(result){
-            projectData = result[0];
-            skills = result[0].skills_used;
-            
-            res.render('projects/edit', {Project: projectData, Skills: skills, Id: query});
+          projectData = result[0];
+          skills = result[0].skills_used;
+          res.render('projects/edit', {Project: projectData, Skills: skills, Id: query});
         }
         else {
-          
           res.redirect('projects');
           console.log("Project Not Found.")
-          
         }
-        
     })
     .catch( function(err) {
         if(err){
             console.log('DB Error:' + err);
         }
     });
-    
 }
 
 // POST Update Project Function
-exports.updateProject = function(req, res, next) { 
-    
+exports.updateProject = function(req, res, next) {
+
      var query = req.params._id;
-  
+
     var proj = req.body;
-    var updateId;
-    var updated;
-    
+
     Project.findOneAndUpdate({_id: mongoose.Types.ObjectId(query)}, { $set: {proj}}, {"new": true}, function(err, doc) {
-      
-    if (err) {
-      
-      console.log("Error", err);
-    }
-    
-    // Updates All Potential Fields
-    
-    if(req.body.name) {
-      
-      doc.name = req.body.name;
-      
-    }
-    
-    if(req.body.pub_date) {
-      
-      doc.pub_date = req.body.pub_date;
-      
-    }
-    
-    if(req.body.proj_img) {
-      
-      doc.proj_img = req.body.proj_img;
-      
-    }
-    
-    if(req.body.proj_desc) {
-      
-      doc.proj_desc = req.body.proj_desc;
-      
-    }
-    
-    if(req.body.proj_link) {
-      
-      doc.proj_link = req.body.proj_link;
-      
-    }
-    
-    if(req.body.demo_link) {
-      
-      doc.demo_link = req.body.demo_link;
-      
-    }
-    
-    if(req.body.code_link) {
-      
-      doc.code_link = req.body.code_link;
-      
-    }
-    
-    
-    
-    doc.save( function(err,result) {
-      
-      console.log(result);
+
+      if (err) {
+        console.log("Error", err);
+      }
+
+      // Updates All Potential Fields
+      (req.body.name) ? doc.name = req.body.name : null;
+      (req.body.pub_date) ? doc.pub_date = req.body.pub_date : null;
+      (req.body.proj_img) ? doc.proj_img = req.body.proj_img : null;
+      (req.body.proj_desc) ? doc.proj_desc = req.body.proj_desc : null;
+      (req.body.proj_link) ? doc.proj_link = req.body.proj_link : null;
+      (req.body.demo_link) ? doc.demo_link = req.body.demo_link : null;
+      (req.body.code_link) ? doc.code_link = req.body.code_link : null;
+
+      doc.save( function(err,result) {
+        console.log(result);
+      })
+
     })
-    
-    
-    })
-    
     .catch( function(err) {
       if(err){
           console.log('DB Error:' + err);
       }
     });
-    
+
     res.redirect('/projects/'+ query);
-    
 }
 
-
-
-//////////////// ADMIN BLOG FUNCTIONS /////////////////////////////////////// 
+//////////////// ADMIN BLOG FUNCTIONS ///////////////////////////////////////
 
 // GET Blogs Function
-exports.getBlogs = function(req, res, next) { 
-    
-    
-    
+exports.getBlogs = function(req, res, next) {
 }
 
 // GET Blog's posts Function
-exports.getBlogPosts = function(req, res, next) { 
-    
-    
-    
+exports.getBlogPosts = function(req, res, next) {
 }
 
 
 // GET Post Function
-exports.getPost = function(req, res, next) { 
-    
-    
-    
+exports.getPost = function(req, res, next) {
 }
-
 
 
 //////// BLOG FUNCTIONS ///////////
 
 // GET New Blog Function
-exports.newBlog = function(req, res, next) { 
-    
-    
-    
+exports.newBlog = function(req, res, next) {
 }
 
 // POST Add Blog Function
-exports.addBlog = function(req, res, next) { 
-    
-    
-    
+exports.addBlog = function(req, res, next) {
 }
 
 
 // GET Edit Blog Function
-exports.editBlog = function(req, res, next) { 
-    
-    
-    
+exports.editBlog = function(req, res, next) {
 }
 
 // POST Update Blog Function
-exports.updateBlog = function(req, res, next) { 
-    
-    
-    
+exports.updateBlog = function(req, res, next) {
 }
 
 
 //////// POST FUNCTIONS ///////////
 
 // GET New Post Function
-exports.newPost = function(req, res, next) { 
-    
-    
-    
+exports.newPost = function(req, res, next) {
 }
 
 // POST Add Post Function
-exports.addPost = function(req, res, next) { 
-    
-    
-    
+exports.addPost = function(req, res, next) {
 }
 
 
 // GET Edit Post Function
-exports.editPost = function(req, res, next) { 
-    
-    
-    
+exports.editPost = function(req, res, next) {
 }
 
 // POST Update Post Function
-exports.updatePost = function(req, res, next) { 
-    
-    
-    
+exports.updatePost = function(req, res, next) {
 }
--- controllers/projCon.js
+++ controllers/projCon.js
@@ -1,47 +1,37 @@
-
 var mongoose = require("mongoose");
-
 var Project = require("../models/project.js");
-
 var User = require("../models/user.js");
 
 
-
 // Get All Projects
-exports.getAll = function(req, res, next) { 
+exports.getAll = function(req, res, next) {
     var foundprojects;
-   
+
     if(req.query.viewtype) {
       viewtype = req.query.viewtype;
     }
     else {
       viewtype = "card";
     }
-    
+
     Project.find()
-    
-    .then(function(doc) {
-        
+    .then((doc) => {
         if (doc) {
             foundprojects = doc;
-            res.render('projects', {Title: "Projects", Project: foundprojects, View: viewtype});   
+            res.render('projects', {Title: "Projects", Project: foundprojects, View: viewtype});
         }
-        
-     
-    
     })
-    .catch( function(err) {
+    .catch((err) => {
         if(err){
             console.log('DB Error: ' + err);
         }
     });
-    
+
 }
 
 
 // Sign Up Function
-exports.getProjectByID = function(req, res, next) { 
-    
+exports.getProjectByID = function(req, res, next) {
     var projectData;
     var skills;
     var query = req.params._id;
@@ -51,86 +41,65 @@ exports.getProjectByID = function(req, res, next) {
     var day = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
     var mon = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
     var scrip;
-    
-  
+
     Project.find({ _id: query})
-    
-    .then( function(result) {
-        
+    .then((result) => {
         if(result.length > 0){
             projectData = result[0];
             skills = projectData.skills_used;
             sliderimgs = projectData.img_gallery;
-            
             dateString = new Date(projectData.pub_date);
-            
-            if(dateString.getUTCDate() === 1) { 
+
+            if(dateString.getUTCDate() === 1) {
                 scrip = "st";
-                
             } else if (dateString.getUTCDate() === 2) {
                 scrip = "nd";
-                
             } else if (dateString.getUTCDate() === 3) {
                 scrip = "rd";
-                
             } else {
                 scrip = "th";
-                
             }
-            
+
             projDate = `${day[dateString.getUTCDay()]} ${mon[dateString.getUTCMonth()]} ${dateString.getUTCDate()}<sup>${scrip}</sup>, ${dateString.getFullYear()}`;
-            
-            
             res.render('project', {Title: "Projects", Proj: projectData, Skills: skills, Slider: sliderimgs, Pub: projDate});
         }
         else {
-          
           res.redirect('projects');
           console.log("Project Not Found.")
-          
         }
-        
     })
-    .catch( function(err) {
+    .catch((err) => {
         if(err){
             console.log('DB Error:' + err);
         }
     });
-    
+
 }
 
-
-
 // Sign Up Function
-exports.getProjectsBySkill = function(req, res, next) { 
-    
+exports.getProjectsBySkill = function(req, res, next) {
+
     var skillquery = req.query.skill;
     var viewtype = req.query.viewtype;
-    
+
     // Find Projects that contain "skill" in "skills_used"
     Project.find({
         'skills_used.skill': skillquery
     })
-    
-    .then( function(result) {
-      
+    .then((result) => {
       if(result){
-          
-          res.render('projects', {Title: "Projects", Project: result, View: viewtype});
+        res.render('projects', {Title: "Projects", Project: result, View: viewtype});
       }
       else {
-        
         res.redirect('home');
         console.log("Project Not Found.")
-        
       }
-        
     })
-    .catch( function(err) {
+    .catch((err) => {
         if(err){
             console.log('DB Error:' + err);
         }
     });
-    
+
 }
 
